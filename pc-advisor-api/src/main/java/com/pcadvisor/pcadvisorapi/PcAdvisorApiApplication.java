6e27f9e649aeb2356378e8fffc8092d0041c43ad
Petar Cerovic <quadrition@gmail.com>
2021-05-16 13:12:28 +0200

    Added initialized spring boot project

---
+++ pc-advisor-api/src/main/java/com/pcadvisor/pcadvisorapi/PcAdvisorApiApplication.java
@@ -0,0 +1,13 @@
+package com.pcadvisor.pcadvisorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcAdvisorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcAdvisorApiApplication.class, args);
+	}
+
+}
---
+++ pc-advisor-api/src/test/java/com/pcadvisor/pcadvisorapi/PcAdvisorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcadvisor.pcadvisorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcAdvisorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
