e66b8fe7365538ebfabf0e226bb847d8141f3b8a
Mikec <mikecitt@gmail.com>
2021-05-29 12:20:09 +0200

    Added separate kjar project for rules.

--- pc-advisor-api/src/main/java/com/pcadvisor/pcadvisorapi/PcAdvisorApiApplication.java
+++ pc-advisor-api/src/main/java/com/pcadvisor/pcadvisorapi/PcAdvisorApiApplication.java
@@ -1,22 +1,26 @@
 package com.pcadvisor.pcadvisorapi;
 
-import com.pcadvisor.pcadvisorapi.model.PCBuild;
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class PcAdvisorApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PcAdvisorApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PcAdvisorApiApplication.class, args);
+    }
 
-	@Bean
-	public KieContainer kieContainer() {
-		return KieServices.Factory.get().getKieClasspathContainer();
-	}
+    @Bean
+    public KieContainer getKieContainerInstance() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("org.pcadvisorkjar", "pc-advisor-kjar", "1.0-SNAPSHOT"));
+        KieScanner kScanner = ks.newKieScanner(kContainer);
+        // kScanner.start(10_000);
+        return kContainer;
+    }
 }
